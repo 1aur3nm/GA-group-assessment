cb929ef40eae787dc8c1a9d53eb683ddf9aebc20
Norman Philipp <114048542+2bseen@users.noreply.github.com>
2022-11-23 14:16:34 +1100

    added basis for validation and form input

--- index.js
+++ index.js
@@ -5,3 +5,36 @@ var dropdownElementList = [].slice.call(
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
   return new bootstrap.Dropdown(dropdownToggleEl);
 });
+const saveChanges
+document.addEventListener("click", saveChanges);
+function saveChanges() {
+  document.getElementsByClassName(btnbtn-primary).addEventListener("click"), saveChanges;
+};
+
+submitButton.addEventListener("click", () => saveChanges);
+const submitButton = document.getElementsByClassName("btn btn-primary")
+
+
+
+    ///Form Validation Code
+const validateTaskForm,
+  // validation fails if the input is blank
+  {if:(form.inputfield.value == <"8"); {
+    alert ("Error: Input is empty!")
+    form.inputfield.focus();
+    return false;
+  }
+
+  // regular expression to match only alphanumeric characters and spaces
+  var re = /^[\w ]+$/;
+
+  // validation fails if the input doesn't match our regular expression
+  if(!re.test(form.inputfield.value)) {
+    alert("Error: Input contains invalid characters!");
+    form.inputfield.focus();
+    return false;
+  }
+
+  // validation was successful
+  return true;
+;
