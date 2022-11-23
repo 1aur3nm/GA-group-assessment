4adc172bd7b66adeb8c4c5e3ac8c0228608c9ed3
Lauren B <bulleylauren@gmail.com>
2022-11-23 14:10:30 +1100

    Added Date and time function to JS | Added id in HTML | Added little style li element

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ var dropdownElementList = [].slice.call(
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
   return new bootstrap.Dropdown(dropdownToggleEl);
 });
+
+// Adding Day and time
+function formatDateAndTime(timestamp) {
+  let date = new Date(timestamp);
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let mins = date.getMinutes();
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  console.log(date);
+  return (date = `${day} ${hours}:${mins}`);
+
+  let newDate = new Date(date);
+  formatDateAndTime(newDate);
+}
