0d27bc8f6df8da57c5b9973e931c1c1cb18f43fa
Lauren B <bulleylauren@gmail.com>
2022-11-27 22:06:15 +1100

    Created Getters and Setter is statements | Added 2 br elements in HTML to move cards down
    Many errors at this stage in code

--- Classes/classes.js
+++ Classes/classes.js
@@ -2,17 +2,70 @@
 ////Modify the TaskManagerclass by adding a method that takes as & an argument a task JSON object and creates a Card Layout HTML as defined in sprint 1
 ////Add an id attribute to the content list group created in sprint 1 and write the code to add the card to your HTML element
 
-
-////Unnamed Class
-class taskManager { // class name: taskManager
+// class name: taskManager
+class taskManager { 
     const taskList = [];
-    constructor taskManagerFunction(name, desription, assign, calendar, status){
-        this.name = name;
-        this.desription = desription;
-        this.assign = assign;
-        this.calendar = calendar;
-        this.status = status;
+    // the objects "setup" | Makes and returns objects
+    constructor taskManagerFunction(name, desription, assign, calendar, status) {
+        this._name = name;
+        this._desription = desription;
+        this._assign = assign;
+        this._calendar = calendar;
+        this._status = status;
+        this._id = 0;
+ }
+
+ ////Getters
+ getName(){
+    return this._name
+ };
+
+ getDesription(){
+    return this._desription
+ };
+
+ geAssign(){
+    return this._assign
+ };
+
+ getCalendar(){
+    return this._calendar
+ };
+
+ getStatus(){
+     return this._status
+ }
+
+ geId(){
+    return this._id
+ };
+
+ ////Setters
+setName (newName){
+    this._name = newName;
 }
+
+setDesription (newDesription){
+    this._desription = newDesription;
+}
+
+setAssign (newAssign){
+    this._assign = newAssign;
+}
+
+setcalendar (newCalendar){
+    this._calendar = newCalendar;
+}
+
+setStatus (newStatus){
+    this._status = newStatus;
+}
+
+setId (newId){
+    this._id = newId;
+}
+
+
+
 }
 //console.log(taskManager.name);
-
