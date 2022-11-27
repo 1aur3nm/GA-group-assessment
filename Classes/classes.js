ddf962ce73f1fde24ac35415a7fc3b107f60437d
Lauren B <bulleylauren@gmail.com>
2022-11-27 18:29:20 +1100

    Created class called TaskManager, added constructor

---
+++ Classes/classes.js
@@ -0,0 +1,18 @@
+//// Create a class called TaskManager - that will create a new object for each task
+////Modify the TaskManagerclass by adding a method that takes as & an argument a task JSON object and creates a Card Layout HTML as defined in sprint 1
+////Add an id attribute to the content list group created in sprint 1 and write the code to add the card to your HTML element
+
+
+////Unnamed Class
+class taskManager { // class name: taskManager
+    const taskList = [];
+    constructor taskManagerFunction(name, desription, assign, calendar, status){
+        this.name = name;
+        this.desription = desription;
+        this.assign = assign;
+        this.calendar = calendar;
+        this.status = status;
+}
+}
+//console.log(taskManager.name);
+
